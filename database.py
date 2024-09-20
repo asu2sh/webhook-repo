598a291fe73909fc77073b0da6240cd01ee4386a
ashu210 <ashutosh@solivatech.com>
2024-09-21 04:04:37 +0530

    fetch db creds from .env

--- database.py
+++ database.py
@@ -1,21 +1,26 @@
 import os
+from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 
-if os.getenv("ENV") == "production":
-    mongo_uri = os.getenv("MONGO_URI")
-    client = MongoClient(mongo_uri, server_api=ServerApi('1'))
-else:
-    mongo_uri = "mongodb://localhost:27017/"
-    client = MongoClient(mongo_uri)
+load_dotenv()
 
-db = client.github_events
-collection = db.events
+MONGO_REMOTE_URI = os.getenv("MONGO_REMOTE_URI", None)
+MONGO_DATABASE_NAME = os.getenv("MONGO_DATABASE_NAME")
+MONGO_CONNECTION_NAME = os.getenv("MONGO_CONNECTION_NAME")
+
+if MONGO_REMOTE_URI:
+    client = MongoClient(MONGO_REMOTE_URI, server_api=ServerApi('1'))
+else:
+    client = MongoClient("mongodb://localhost:27017/")
+
+db = client[MONGO_DATABASE_NAME]
+collection = db[MONGO_CONNECTION_NAME]
 
 async def store_event_in_db(event):
     try:
-        result = db.events.insert_one(event)
+        result = collection.insert_one(event)
         print(f"Stored event with id: {result.inserted_id}")
     except Exception as e:
         print(f"Error storing event in MongoDB: {str(e)}")
