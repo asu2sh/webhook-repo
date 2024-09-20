7d14bf11121fc67da33932c1758721bb9393c06e
ashu210 <ashutosh@solivatech.com>
2024-09-21 04:10:06 +0530

    fetch db creds from .env

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 MONGO_REMOTE_URI = os.getenv("MONGO_REMOTE_URI", None)
 MONGO_DATABASE_NAME = os.getenv("MONGO_DATABASE_NAME")
-MONGO_CONNECTION_NAME = os.getenv("MONGO_CONNECTION_NAME")
+MONGO_COLLECTION_NAME = os.getenv("MONGO_COLLECTION_NAME")
 
 if MONGO_REMOTE_URI:
     client = MongoClient(MONGO_REMOTE_URI, server_api=ServerApi('1'))
@@ -16,7 +16,7 @@ else:
     client = MongoClient("mongodb://localhost:27017/")
 
 db = client[MONGO_DATABASE_NAME]
-collection = db[MONGO_CONNECTION_NAME]
+collection = db[MONGO_COLLECTION_NAME]
 
 async def store_event_in_db(event):
     try:
